ac214c1854e571b312aa2ac1cc2c54ace2233606
Gregory Kononovitch <gregory.kononovitch@gmail.com>
2011-10-11 16:38:13 +0000

    org
    Signed-off-by: Gregory Kononovitch <gregory.kononovitch@gmail.com>

--- c642/c642_internal.h
+++ c642/c642_internal.h
@@ -10,5 +10,27 @@
 #ifndef C642_INTERNAL_H_
 #define C642_INTERNAL_H_
 
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+
+#include <linux/err.h>
+#include <linux/list.h>
+#include <linux/atomic.h>
+#include <linux/spinlock.h>
+
+#include <linux/firmware.h>
+//
+#define A051_PROFILE
+#define A051_PROFILE_MEM
+//#define A051_PROFILE_TXT
+extern char   a051_code[17];
+extern struct a051_proc_env *a051_env;
+extern struct a051_log_env  *a051_log;
+#include "../../a051/src/a051_profile.h"
+
+
+
 
 #endif /* C642_INTERNAL_H_ */
--- c642/c642_module.c
+++ c642/c642_module.c
@@ -7,3 +7,39 @@
  * There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#include "c642_internal.h"
+
+char a051_code[17];
+struct a051_proc_env *a051_env = NULL;
+struct a051_log_env *a051_log = NULL;
+
+
+
+
+
+/*
+ *
+ */
+int c642_init(void)
+{
+    int r;
+    ainit("bird", "pict", 32, A051_LOG_MEM, "C642", 4, 25);
+
+    areturn(0, "ok");
+}
+
+/*
+ *
+ */
+void c642_exit(void)
+{
+    astart(1, " ");
+
+    aquitv("QUIT");
+}
+
+module_init(c642_init);
+module_exit(c642_exit);
+
+MODULE_AUTHOR("Gregory Kononovitch <gregory.kononovitch@gmail.com>");
+MODULE_LICENSE("GPL");
