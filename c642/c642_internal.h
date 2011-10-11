b6ae417b1507b5f2e737cada0c48154b40397a94
Gregory Kononovitch <gregory.kononovitch@gmail.com>
2011-10-11 16:18:54 +0000

    Init c642-pictu image module
    Signed-off-by: Gregory Kononovitch <gregory.kononovitch@gmail.com>

---
+++ c642/c642_internal.h
@@ -0,0 +1,14 @@
+/*
+ * c642_internal.h
+ *
+ * Date Oct 11, 2011
+ * Author and copyright (C) 2011 : Gregory Kononovitch
+ * License : GNU Library or "Lesser" General Public License version 3.0 (LGPLv3)
+ * There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef C642_INTERNAL_H_
+#define C642_INTERNAL_H_
+
+
+#endif /* C642_INTERNAL_H_ */
---
+++ c642/c642_module.c
@@ -0,0 +1,9 @@
+/*
+ * c642_module.c
+ *
+ * Date Oct 11, 2011
+ * Author and copyright (C) 2011 : Gregory Kononovitch
+ * License : GNU Library or "Lesser" General Public License version 3.0 (LGPLv3)
+ * There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
