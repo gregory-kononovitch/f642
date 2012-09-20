875f1e128df0e0437ea9bb466002def4feb5a613
Gregory Kononovitch <gregory.kononovitch@gmail.com>
2012-09-20 05:31:24 +0000

    asm hdcl & include
    Signed-off-by: Gregory Kononovitch <gregory.kononovitch@gmail.com>

--- f645/inclu/hufdcl-1.s
+++ f645/inclu/hufdcl-1.s
@@ -22,8 +22,8 @@
 				shl 		bits, 1
 				jc			.case1x
 				; 0x
+				jo			.case01x
 				shl			bits, 1
-				jc			.case01x
 				; 00
 				sub			off, 2
 				xor			symb, symb
@@ -31,7 +31,7 @@
 				;
 				; 01x
 .case01x:
-				shl			bits, 1
+				shl			bits, 2
 				jc			.case011
 				; 010
 				sub			off, 3
@@ -45,10 +45,9 @@
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;
 .case1x:
-				shl			bits, 1
-				jc			.case11x
+				jno			.case11x
 				; 10x
-				shl			bits, 1
+				shl			bits, 2
 				jc			.case101
 				; 100
 				sub			off, 3
@@ -62,7 +61,7 @@
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .case11x:
-				shl			bits, 1
+				shl			bits, 2
 				jc			.case111x
 				; 110
 				sub			off, 3
@@ -73,25 +72,25 @@
 .case111x:
 				shl			bits, 1
 				jc			.case4x
-				; 110
+				; 1110
 				sub			off, 4
 				mov			symb, 6
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .case4x:
+				jno			.case5x
+				; 11110
 				shl			bits, 1
-				jc			.case5x
-				; 110
 				sub			off, 5
 				mov			symb, 7
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .case5x:
-				shl			bits, 1
+				shl			bits, 2
 				jc			.case6x
-				; 110
+				; 111110
 				sub			off, 6
 				mov			symb, 8
 				jmp			.donedcl
@@ -100,29 +99,34 @@
 .case6x:
 				shl			bits, 1
 				jc			.case7x
-				; 110
+				; 1111110
 				sub			off, 7
 				mov			symb, 9
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .case7x:
+				jno			.case8x
 				shl			bits, 1
-				jc			.case8x
-				; 110
+				; 11111110
 				sub			off, 8
 				mov			symb, 10
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 .case8x:
-				shl			bits, 1
-				jc			.err
-				; 110
+				shl			bits, 2
+				jc			.caseerr
+				; 111111110
 				sub			off, 9
 				mov			symb, 11
 				jmp			.donedcl
 				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
+.caseerr:
+				sub			off, 9
+				jmp			.err
+				;
+				;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
