e8a15ba3c2a94089b010c037a59ef8072c034cf0
Gregory Kononovitch <gregory.kononovitch@gmail.com>
2012-09-20 06:41:58 +0000

    prepare asm mjpeg
    Signed-off-by: Gregory Kononovitch <gregory.kononovitch@gmail.com>

--- f645/mjpeg2.asm
+++ f645/mjpeg2.asm
@@ -2,7 +2,7 @@
 ; file    : mjpeg2.asm
 ; project : f645 (t508.f640/2)
 ;
-; Created on: Sep 18, 2012
+; Created on: Sep 19, 2012
 ; Author and copyright (C) 2012 : Gregory Kononovitch
 ; License : GNU Library or "Lesser" General Public License version 3.0 (LGPLv3)
 ; There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -27,7 +27,7 @@ global 	hu2fman645:			function
 
 
 SECTION .data		ALIGN=16
-;%include "mjpeg_tables.ash"
+;%include "inclu/mjpeg_tables.s"
 
 ALIGN 16
 
@@ -36,15 +36,20 @@ ALIGN 16
 SECTION .text		ALIGN=16
 
 
-
-%macro	shift1 0
-		sub			off, 1
-		jns			.loop
-		movbe		r9, qword [data]
-		add			data, 8
-		mov			off, 63
-		jmp			.loop
-%endmacro
+;;;;;;;;;;;;;;;
+; STACK
+%define		VAR			0x0100
+%define		TREEL		0x17D0
+%define		TREEC		0x2EA0
+%define		QUANY		0x2FA0
+%define		QUANC		0x30A0
+; Local variables (RBP - VAR + )
+%define		PTREEL		0x00
+%define		PTREEC		0x08
+%define		PQUANL		0x10
+%define		PQUANC		0x18
+;
+%define		PTEST		0x80
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; extern long hu2fman645(void *dseg, int size, void *dest);
